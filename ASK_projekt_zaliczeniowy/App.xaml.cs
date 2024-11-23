41f1f8bd257412f3c1b2e807db6f14ca4c481176
DIDUS <didusogiel@gmail.com>
2024-11-23 21:23:21 +0100

    Setup Project
    Basic Project Setup
    :)

---
+++ ASK_projekt_zaliczeniowy/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ASK_projekt_zaliczeniowy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ASK_projekt_zaliczeniowy/CPU.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASK_projekt_zaliczeniowy
+{
+    public class CPU
+    {
+        string ax, bx, cx, dx;
+        string si, di, bp, disp;
+        string XCHG;
+        string[] TABLICA = new string[65536];
+        string starszyAdres, mlodszyAdres;
+        int siDec, diDec, dispDec, sumDec;
+        int bxDec, bpDec;
+        string sumHex, pobierz, odczyt, zamiana;
+
+        string[] STOS = new string[65536];
+        int wskaznikStosu = 0;
+
+
+    }
+}
