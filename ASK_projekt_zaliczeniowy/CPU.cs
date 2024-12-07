094fe815fb05a462136bf373ba881c053ebe585d
DIDUS <didusogiel@gmail.com>
2024-12-07 09:52:42 +0100

    setup1
    setup basic layout & variables

--- ASK_projekt_zaliczeniowy/CPU.cs
+++ ASK_projekt_zaliczeniowy/CPU.cs
@@ -8,13 +8,17 @@ namespace ASK_projekt_zaliczeniowy
 {
     public class CPU
     {
+        // indeksy
         string ax, bx, cx, dx;
         string si, di, bp, disp;
+        
         string XCHG;
-        string[] TABLICA = new string[65536];
+        string[] TABLICA = new string[65536]; // pamięć
         string starszyAdres, mlodszyAdres;
+        
         int siDec, diDec, dispDec, sumDec;
         int bxDec, bpDec;
+        
         string sumHex, pobierz, odczyt, zamiana;
 
         string[] STOS = new string[65536];
